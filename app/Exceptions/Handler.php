e4cbc8822000c5d9b85d02f7a9dc6e13b01d751b
Amit <amit@culture-red.com>
2023-09-07 14:11:21 +0530

    Token Expire handle exception added

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Http\Response;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,5 +48,21 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof AuthenticationException) {
+            return response()->json(
+                [
+                    'type' => 'error',
+                    'status' => Response::HTTP_UNAUTHORIZED,
+                    'message' => 'Access Token expires',
+                ],
+                Response::HTTP_UNAUTHORIZED
+            );
+        }
+
+        return parent::render($request, $e);
     }
 }
