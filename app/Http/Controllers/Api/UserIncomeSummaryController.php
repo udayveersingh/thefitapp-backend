e6c3a265d5d8986207a0306fb7be6e2a207d81f7
Uday <uday.jbbn@gmail.com>
2023-10-13 13:34:38 +0530

    verify profile process

--- app/Http/Controllers/Api/UserIncomeSummaryController.php
+++ app/Http/Controllers/Api/UserIncomeSummaryController.php
@@ -412,13 +412,13 @@ class UserIncomeSummaryController extends Controller
             $kyc_doc_pan= null;
             if($request->hasFile('kyc_doc_pan')){
                 $kyc_doc_pan = 'pan-'.time() . '.' . $request->kyc_doc_pan. '.' . $request->kyc_doc_pan->extension();
-                $request->kyc_doc_1->move(public_path('storage/user/'.$user->id.'/'),  $kyc_doc_pan);
+                $request->kyc_doc_pan->move(public_path('storage/user/'.$user->id.'/'),  $kyc_doc_pan);
             }
 
             $kyc_doc_aadhar= null;
             if($request->hasFile('kyc_doc_aadhar')){
                 $kyc_doc_aadhar = 'aadhar-'.time() . '.' . $request->kyc_doc_aadhar. '.'. $request->kyc_doc_aadhar->extension();
-                $request->kyc_doc_2->move(public_path('storage/user/'.$user->id.'/'),  $kyc_doc_aadhar);
+                $request->kyc_doc_aadhar->move(public_path('storage/user/'.$user->id.'/'),  $kyc_doc_aadhar);
             }
 
             $user_profile->user_id = $user->id;
